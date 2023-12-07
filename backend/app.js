1f6f500c08e3baa6b99f8a31eee8bf6d43d3f117
Shuaib Siddiqui <sbsidd17@gmail.com>
2023-12-07 17:00:53 +0530

    updated and added contactUs page

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,7 @@ import morgan from 'morgan';
 import userRoutes from './routes/user.routes.js'; 
 import courseRoutes from './routes/course.routes.js'; 
 import paymentRoutes from './routes/payment.routes.js';
+import miscellaneousRoutes from './routes/miscellaneous.routes.js';
 import express from 'express';
 import connectToDb from './config/db.config.js';
 import errorMiddleware from './middleware/error.middleware.js';
@@ -18,18 +19,19 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(morgan('dev'));
 app.use(cors({ origin: [process.env.CLIENT_URL], credentials: true }));
-app.use(errorMiddleware);
 
 
 app.use('/api/v1/user', userRoutes); 
 app.use('/api/v1/courses', courseRoutes); 
 app.use('/api/v1/payments', paymentRoutes);
+app.use('/api/v1/', miscellaneousRoutes);
  
 
 app.all('*', (req, res) => {
     res.status(404).send('OOPS!! 404 page not found');
 })
 
+app.use(errorMiddleware);
 
 // db init
 connectToDb();
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,8 @@ import AboutUs from "./Pages/About";
 import NotFound from './Pages/NotFound';
 import Signup from "./Pages/Signup";
 import Login from "./Pages/Login";
+import CourseList from "./Pages/Course/CourseList";
+import Contact from "./Pages/Contact";
 
 function App() {
   return (
@@ -13,8 +15,11 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/about" element={<AboutUs />} /> 
+        <Route path="/contact" element={<Contact />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
+
+        <Route path="/courses" element={<CourseList />} />
 
         <Route path="*" element={<NotFound />} />
       </Routes>
