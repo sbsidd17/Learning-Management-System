12dd90baf1b5eda8f5ad66858370d7a00ea64785
Shuaib Siddiqui <sbsidd17@gmail.com>
2023-12-07 16:30:57 +0530

    added login, logout

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,16 +3,18 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import HomePage from "./Pages/HomePage";
 import AboutUs from "./Pages/About";
-import NotFound from "./Pages/NotFound";
+import NotFound from './Pages/NotFound';
 import Signup from "./Pages/Signup";
+import Login from "./Pages/Login";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/about" element={<AboutUs />} />
+        <Route path="/about" element={<AboutUs />} /> 
         <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
 
         <Route path="*" element={<NotFound />} />
       </Routes>
