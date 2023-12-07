ccb360e47104e26840a5bcd3f35db356bafabf44
Shuaib Siddiqui <sbsidd17@gmail.com>
2023-12-07 16:17:30 +0530

    SignUp completed

--- client/src/Redux/Slices/AuthSlice.js
+++ client/src/Redux/Slices/AuthSlice.js
@@ -1,16 +1,33 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import toast from "react-hot-toast";
+import { axiosInstance } from '../../Helpers/axiosInstance';
 
 const initialState = {
-  isLoggedIn: localStorage.getItem("isLoggedIn") || false,
-  role: localStorage.getItem("role") || "",
-  data: localStorage.getItem("data") || {},
-};
+    isLoggedIn: localStorage.getItem("isLoggedIn") || false,
+    role: localStorage.getItem("role") || "",
+    data: localStorage.getItem("data") || {}
+}
+
+// .....signup.........
+export const createAccount = createAsyncThunk("/auth/signup", async (data) => {
+    const loadingMessage = toast.loading("Please wait! creating your account");
+    try {
+        const res = await axiosInstance.post("/user/register",
+            data,
+            { headers: { 'Content-Type': 'multipart/form-data' } });
+        toast.success(res?.data?.message, { id: loadingMessage });
+        return res?.data
+    } catch (error) {
+        toast.error(error?.response?.data?.message, { id: loadingMessage });
+        throw error;
+    }
+})
 
 const authSlice = createSlice({
-  name: "auth",
-  initialState,
-  reducers: {},
-});
+    name: "auth",
+    initialState,
+    reducers: {}
+})
 
-// export const {} = authSlice.actions;
+export const { } = authSlice.actions;
 export default authSlice.reducer;
